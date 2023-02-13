a574f73fae761b4040acba5d8ad2a054b254d77b
diantnugraha <diantn.nugraha@gmail.com>
2023-02-13 17:51:28 +0700

    initial db schema

---
+++ seeders/20230213103042-UserSeed.js
@@ -0,0 +1,33 @@
+"use strict";
+const db = require("../db/db.json");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    let insertDB = db.map((el) => {
+      delete el.id, (el.createdAt = new Date());
+      el.updatedAt = new Date();
+      return el;
+    });
+
+    await queryInterface.bulkInsert("Users", insertDB);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
